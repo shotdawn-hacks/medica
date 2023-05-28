aaf9028a3b83a45fc75e3c58b411062f32aea93e
hexzedels <hexpisos@gmail.com>
2023-05-28 15:09:45 +0300

    Add classify endpoint

--- microservices/analyzer/server.py
+++ microservices/analyzer/server.py
@@ -1,13 +1,23 @@
-from fastapi import FastAPI
+from fastapi import Request, FastAPI
 import uvicorn
+from DataProcessor import DataProcessor
+import json
 
 app = FastAPI()
 
+processor = DataProcessor("./files/data_standart_sppvr.csv","./files/data_standart_sppvr.csv")
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
+@app.post("/classify")
+async def classify(request: Request):
+    req_body = await request.json()
+    processor.load_data(req_body)
+    processor.process_data()
+    
+    return processor.get_result()
 
 
 if __name__ == "__main__":
